8a4ffa8fb6ba4eeff14eaddb12ebed330280c8cc
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2017-11-18 17:48:15 -0200

    - Iniciando o projeto  NETCORE/ANGULAR 4,5.....  na versão 2.0 do CORE!

--- CoreTeste/Startup.cs
+++ CoreTeste/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CoreTeste.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreTeste
 {
@@ -22,6 +24,7 @@ namespace CoreTeste
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CoreTesteDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
         }
 
