f92b8bf02ff82a08a82b2587265c0ef86edf0ea4
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2017-11-19 18:03:53 -0200

    - Adicionando Resources pra trablhar no API

---
+++ CoreTeste/Persistence/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using CoreTeste.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTeste.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CoreTesteDbContext context;
+
+        public UnitOfWork(CoreTesteDbContext context)
+        {
+            this.context = context;
+        }
+        public Task CompleteAsyc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
